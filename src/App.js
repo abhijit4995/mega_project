d5c549d5c0afd3cc94d3c8cfb49552b9f4866ab8
unknown <rehanmomin796@gmail.com>
2024-08-30 17:58:43 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from '../src/assests/logo.jpg';
 import './App.css';
 import Header from '../src/Components/Layout/header.js';
+import Footer from '../src/Components/Layout/footer.js';
+import Home from '../src/Components/Home/index.js';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
     <div className="App">
       <img src={logo}></img>
     </div>
+    <Home/>
+    <Footer/>
     </>
   );
 }
---
+++ src/Components/Layout/footer.js
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer>
+            Footer section
+        </footer>
+    )
+}
+export default Footer;
