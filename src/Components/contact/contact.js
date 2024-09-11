e056943121639cf13c2039d80bb7e33b84976ef4
unknown <rehanmomin796@gmail.com>
2024-09-11 18:00:55 +0530

    msg

--- src/Components/contact/contact.js
+++ src/Components/contact/contact.js
@@ -1,8 +1,53 @@
 import React from 'react'
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+    <h2>Contact</h2>
+    <Row>
+        <Form.Label column="lg" lg={2}>
+          Name :
+        </Form.Label>
+        <Col>
+          <Form.Control size="lg" type="text" placeholder="Enter your Name" />
+        </Col>
+    </Row>
+      <br />
+      <Row>
+        <Form.Label column="lg" lg={2}>
+          Email :
+        </Form.Label>
+        <Col>
+          <Form.Control size="lg" type="email" placeholder="Enter your Email" />
+        </Col>
+    </Row>
+      <br />
+      <Row>
+        <Form.Label column="lg" lg={2}>
+          Number :
+        </Form.Label>
+        <Col>
+          <Form.Control size="lg" type="text" placeholder="Enter your Number" />
+        </Col>
+    </Row>
+      <br />
+      <Row>
+        <Form.Label column="lg" lg={2}>
+          Text :
+        </Form.Label>
+        <Col>
+          <Form.Control size="lg" type="text" placeholder="Enter text" />
+        </Col>
+      </Row>
+      <br />
+      <Button type="submit" className="mb-2">
+            Submit
+      </Button>
+    </>
   )
 }
 
